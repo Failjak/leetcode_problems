bfb40ebec0a56ccf569a7456da35b9f2f42b65a7
failjak <nikita.gushtyn200370@gmail.com>
2022-07-09 21:11:06 +0300

    Solved #70 climbingStairs

--- climbStairs.py
+++ climbStairs.py
@@ -2,10 +2,28 @@ from test_cases import test_cases
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n in (1, 2, 3):
-            return n
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        """
+        mem: O(1)
+        compl: O(n)
+        """
 
+        one, two = 1, 1
+        for _ in range(n - 1):
+            tmp = one
+            one += two
+            two = tmp
+
+        return one
+
+    def graphClimgStairs(self, n: int) -> int:
+        i = 0
+        d1, d2 = 1, 2
+        queue = {0: [1, 2]}
+
+        for i in range(n):
+            neibs = queue.get(i, 0)
+            for neib in neibs:
+                queue[neib] = neib + d1, neib + d2
 
 """
 1: 1
@@ -57,7 +75,7 @@ test_cases(
     params=[
         (2, ),
         (3, ),
-        (45, )
+        (5, ),
     ],
-    answers=[2, 3, 0]
+    answers=[2, 3, 8]
 )
