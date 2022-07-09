e9a01a6012434c6c0520c5fb1118389a3a4dfa62
failjak <nikita.gushtyn200370@gmail.com>
2022-07-09 14:03:31 +0300

    fixed test_cases func

--- test_cases.py
+++ test_cases.py
@@ -1,7 +1,7 @@
 def test_cases(func, cases: list):
     flag = True
     for case, answ in cases:
-        res = func(case)
+        res = func(*case)
         if not res == answ: 
             flag = False
             print(f"Case: {case}; Result: {res}; Right answer: {answ}")
