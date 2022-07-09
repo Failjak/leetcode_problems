178650b2182365d2f236994bb3451e628b87cde5
failjak <nikita.gushtyn200370@gmail.com>
2022-07-09 14:03:57 +0300

    Realized the binarySearch

---
+++ binarySearch.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+from test_cases import test_cases
+
+class Solution:
+    def binarySearch(self, nums: List[int], to_find: int) -> int:
+        n = len(nums)
+        left, rigth = 0, n - 1
+
+        while left <= rigth:
+            midd = (rigth + left) // 2
+            if nums[midd] > to_find:
+                rigth = midd - 1
+            elif nums[midd] < to_find:
+                left = midd + 1
+            else:
+                return midd
+        
+        return -1
+
+
+
+test_cases(
+    func=Solution().binarySearch,
+    cases=[
+        (([1, 5, 6, 9, 15, 22, 28], 1), 0),
+        (([2, 3, 5, 7, 9, 13, 14, 20, 23], 23), 8),
+    ]
+)
