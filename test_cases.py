990ce992755f60250eb1fb7f9cd5530c162a26cc
failjak <nikita.gushtyn200370@gmail.com>
2022-07-09 14:25:47 +0300

    upgraded test_cases func

--- test_cases.py
+++ test_cases.py
@@ -1,9 +1,22 @@
-def test_cases(func, cases: list):
-    flag = True
-    for case, answ in cases:
-        res = func(*case)
+def test_cases(func, keyses: list, params: list, answers: list):
+    n = len(params)
+    if n != len(answers):
+        print("Failed: Invalid params!")
+        return 
+
+    traceback = []
+    for i in range(n):
+        
+        kwargs = dict(zip(keyses, params[i]))
+        res = func(**kwargs)
+        answ = answers[i]
+
         if not res == answ: 
-            flag = False
-            print(f"Case: {case}; Result: {res}; Right answer: {answ}")
-    if flag:
+            traceback.append(f"Case: {kwargs}; Result: {res}; Right answer: {answ}")
+    
+    if traceback:
+        print("Failed:")
+        for msg in traceback:
+            print(msg)
+    else:
         print("Cases done sucessful!!")
