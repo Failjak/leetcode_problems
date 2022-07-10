a93c85fb9c616c0feabfb4e549db076352793e9e
failjak <nikita.gushtyn200370@gmail.com>
2022-07-10 21:00:12 +0300

    Solved #303 RangeSumQuery

--- maxSubArray.py
+++ maxSubArray.py
@@ -1,32 +1,29 @@
 from typing import List
 
-
-from test_cases import test_cases
-
-# 53. Maximum Subarray
-# https://leetcode.com/problems/maximum-subarray/
+# 303. Range Sum Query - Immutable
+# https://leetcode.com/problems/range-sum-query-immutable/
 
 
-class Solution:
+class NumArray:
 
-    def maxSubArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        sum_so_far = curr_max = nums[0]
-
-        for i in range(1, n):
-            curr_max = max(nums[i], curr_max + nums[i])
-            sum_so_far = max(sum_so_far, curr_max)
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.cache = {}
+    
+    def sumRange(self, left: int, right: int) -> int:
+        left_sum = right_sum = 0
         
-        return sum_so_far
+        if not self.cache.get(left):
+            self.cache[left] = sum(self.nums[:left + 1])
+        left_sum = self.cache[left]
+        
+        if not self.cache.get(right):
+            self.cache[right] = sum(self.nums[:right + 1])
+        right_sum = self.cache[right]
+        
+        return right_sum - left_sum + self.nums[left]
 
 
-test_cases(
-    func=Solution().maxSubArray,
-    keyses=['nums'],
-    params=[
-        ([-2,1,-3,4,-1,2,1,-5,4], ),
-        ([5,4,-1,7,8], ),
-        ([-1], )
-    ],
-    answers=[6, 23, 1]
-)
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
