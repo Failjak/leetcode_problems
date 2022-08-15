06212bb19ac7e8e677f1145cd9f53f9d3ed0355e
failjak <nikita.gushtyn200370@gmail.com>
2022-08-15 18:33:43 +0300

    Added debug flag to test_cases function

--- test_cases.py
+++ test_cases.py
@@ -1,4 +1,4 @@
-def test_cases(func, keyses: list, params: list, answers: list):
+def test_cases(func, keyses: list, params: list, answers: list, debug: bool = True):
     n = len(params)
     if n != len(answers):
         print("Failed: Invalid params!")
@@ -11,12 +11,12 @@ def test_cases(func, keyses: list, params: list, answers: list):
         res = func(**kwargs)
         answ = answers[i]
 
-        if not res == answ: 
+        if debug and not res == answ: 
             traceback.append(f"Case: {kwargs}; Result: {res}; Right answer: {answ}")
     
-    if traceback:
+    if debug and traceback:
         print("Failed:")
         for msg in traceback:
             print(msg)
-    else:
+    elif debug:
         print("Cases done sucessful!!")
